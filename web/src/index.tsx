ab6cff6bcfe0b72a681ca9308ca6e0d797c9482d
unknown <andrej.germic@gmail.com>
2021-10-05 10:39:50 +0200

    Feat: created default global styles, added google fonts

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import data from "./data/products.json";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
-const App = () => {
-  return <img src={data[0].categoryImage.desktop}></img>;
-};
+import theme from "./data/theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Main theme based on design system
+const GlobalStyle = createGlobalStyle`
+/* Body */
+  body *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+ 
+  /* Default texts */
+  blockquote, dl, dd, h1, h2, h3, h4,
+  h5, h6, hr, figure, p, pre {
+    margin: 0;
+    font-size: inherit;
+    font-weight: inherit;
+  }
+  
+  /* Unstyled lists */
+  ol, ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+/* Unstyled buttons */
+  button {
+    border: none;
+    outline: none;
+  }
+`;
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
