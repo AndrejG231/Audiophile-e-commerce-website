5b568bfeb11e213b1f16c30957778c7124d69ffe
unknown <andrej.germic@gmail.com>
2021-10-06 12:41:25 +0200

    Feat: arrow buttons container

---
+++ web/src/components/buttons.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ButtonColor = styled.button`
+  outline: none;
+  border: none;
+  background: ${({ theme }) => theme.colors.brown};
+  ${({ theme }) => theme.fonts.subtitle};
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 1px;
+  padding: 15px 30px;
+  cursor: pointer;
+  &:hover {
+    background: ${({ theme }) => theme.colors.lightBrown};
+  }
+`;
+
+export const ArrowButtonContainer = styled.button`
+  position: relative;
+  outline: none;
+  border: none;
+  background: transparent;
+  border-radius: 0;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.black};
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 13px;
+  place-items: center;
+  margin: auto;
+  height: 13px;
+  &:hover {
+    color: ${({ theme }) => theme.colors.brown};
+  }
+`;
