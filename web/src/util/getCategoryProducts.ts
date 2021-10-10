b46440ab776c10daa59071d0e6375c39d85d3807
unknown <andrej.germic@gmail.com>
2021-10-10 21:53:33 +0200

    Feat: added products for each category filtering util

---
+++ web/src/util/getCategoryProducts.ts
@@ -0,0 +1,24 @@
+import products from "../data/products";
+
+// Returns list of products for specified category
+const getProducts = (category: string) => {
+  const productsToDisplay: typeof products = [];
+
+  products.forEach((item) => {
+    // Map over all the products data
+    if (item.category !== category) {
+      return;
+    }
+
+    // If current category and new push to beggining, else append to the end of array
+    if (item.new) {
+      return productsToDisplay.unshift(item);
+    }
+
+    return productsToDisplay.push(item);
+  });
+
+  return productsToDisplay;
+};
+
+export default getProducts;
