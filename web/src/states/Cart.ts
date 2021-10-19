1002d6bd7d1a62c41012e6f001b2ad25525e2216
Andrej Germic <andrej.germic@gmail.com>
2021-10-19 18:51:43 +0200

    Feat: added shopping cart state

---
+++ web/src/states/Cart.ts
@@ -0,0 +1,58 @@
+import { Container } from "unstated";
+
+type state = {
+  cart: {
+    [key in string]: {
+      quantity: number;
+    };
+  };
+};
+
+class Card extends Container<state> {
+  state = { cart: {} };
+  addItem = (item: string, quantity: number) => {
+    this.setState((state) => ({
+      cart: {
+        ...state.cart,
+        [item]: {
+          quantity,
+        },
+      },
+    }));
+  };
+  removeItem = (item: string) => {
+    this.setState((state) => ({
+      cart: {
+        ...state.cart,
+        [item]: {
+          quantity: 0,
+        },
+      },
+    }));
+  };
+
+  increaseQuantity = (item: string) => {
+    this.setState((state) => ({
+      cart: {
+        ...state.cart,
+        [item]: {
+          quantity: state.cart[item].quantity + 1,
+        },
+      },
+    }));
+  };
+
+  decreaseQuantity = (item: string) => {
+    this.setState((state) => ({
+      cart: {
+        ...state.cart,
+        [item]: {
+          quantity:
+            state.cart[item].quantity - 1 > 0 ? state.cart[item].quantity : 0,
+        },
+      },
+    }));
+  };
+}
+
+export default Card;
