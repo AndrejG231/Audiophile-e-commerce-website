a3f259908203a4029ea8b54a8feb547a1cd22778
Andrej Germic <andrej.germic@gmail.com>
2021-11-10 06:59:23 +0100

    Refactor: moved input checker function outside main test function scope

--- web/src/states/Checkout.ts
+++ web/src/states/Checkout.ts
@@ -2,6 +2,7 @@ import { Container } from "unstated";
 
 type props = {
   textInputs: {
+    // key - identifier of input, value and errors of input and checks
     [key in string]: {
       value: string;
       error: string;
@@ -9,7 +10,7 @@ type props = {
   };
   boolInputs: {
     [key in string]: string;
-    // (key refers to group of inputs), value - string refers to identifier of input
+    // key refers to group of inputs, value - string refers to identifier of input
   };
 };
 
--- web/src/util/checkoutInputsCheck.ts
+++ web/src/util/checkoutInputsCheck.ts
@@ -1,3 +1,47 @@
+const mailRegex =
+  /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/;
+
+// Some default error checker functions
+const checkWordCount = (value: string, min: number, max = min) => {
+  const length = value.split(" ").length;
+  if (length < min || length > max) {
+    return "Invalid format.";
+  }
+  return null;
+};
+
+const checkLength = (value: string) => {
+  if (value.length < 5) {
+    return "Min. length is 5!";
+  }
+  return null;
+};
+
+const checkPhoneNumber = (value: string) => {
+  if (
+    !/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(
+      value
+    )
+  ) {
+    return "Invalid format.";
+  }
+  return null;
+};
+
+const checkEmail = (value: string) => {
+  if (!mailRegex.test(value)) {
+    return "Invalid format.";
+  }
+  return null;
+};
+
+const checkDigits = (value: string) => {
+  if (!/^\+?\d+$/.test(value)) {
+    return "Digits only!";
+  }
+  return null;
+};
+
 // Checks identifier to input value pairs based on identifier's checking rules
 
 const checkoutInputCheck = (
@@ -9,58 +53,13 @@ const checkoutInputCheck = (
     value = "";
   }
 
-  // Some default error checker functions
-  const checkWordCount = (min: number, max = min) => {
-    const length = value.split(" ").length;
-    if (length < min || length > max) {
-      return "Invalid format.";
-    }
-    return null;
-  };
-
-  const checkLength = () => {
-    if (value.length < 5) {
-      return "Min. length is 5!";
-    }
-    return null;
-  };
-
-  const checkPhoneNumber = () => {
-    if (
-      !/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(
-        value
-      )
-    ) {
-      return "Invalid format.";
-    }
-    return null;
-  };
-
-  const checkEmail = () => {
-    if (
-      !/([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/.test(
-        value
-      )
-    ) {
-      return "Invalid format.";
-    }
-    return null;
-  };
-
-  const checkDigits = () => {
-    if (!/^\+?\d+$/.test(value)) {
-      return "Digits only!";
-    }
-    return null;
-  };
-
   //   Append specific test functions to identifier
   const tests: Function[] = [];
 
   switch (identifier) {
     case "fullname":
       tests.push(checkLength);
-      tests.push(() => checkWordCount(2, 4));
+      tests.push((value: string) => checkWordCount(value, 2, 4));
       break;
     case "phone":
       tests.push(checkLength);
@@ -80,6 +79,7 @@ const checkoutInputCheck = (
       tests.push(checkDigits);
       break;
     case "emoney-pin":
+      tests.push(checkDigits);
       break;
     default:
       tests.push(checkLength);
@@ -87,7 +87,7 @@ const checkoutInputCheck = (
 
   //   Loop through selected tests and return first that does not pass
   for (const testItem of tests) {
-    const result = testItem();
+    const result = testItem(value);
     if (result) {
       return setError(result);
     }
