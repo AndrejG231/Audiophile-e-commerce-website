23dcc10d481ec3e61315db230c141393db466daf
Andrej Germic <andrej.germic@gmail.com>
2021-11-11 17:13:58 +0100

    Fix: checkout form default input values

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,7 +1,19 @@
 export const _SERVER = "http://192.168.100.146:3000/";
 
-export const checkoutId = "checkout-form";
-
 export const inputGroups = {
   payMethod: "formPaymentMethod",
 };
+
+export const inputIdentifiers = [
+  "fullname",
+  "email",
+  "phone",
+  "address",
+  "zip",
+  "city",
+  "country",
+  "paymethod",
+  "select-cash",
+  "emoney",
+  "emoney-pin",
+];
