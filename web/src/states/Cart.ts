bae0a664c4834dc0385b51b2c645a67046e95a49
Andrej Germic <andrej.germic@gmail.com>
2021-11-13 06:05:15 +0100

    Feat: added local storage cart items synchronization

--- web/src/states/Cart.ts
+++ web/src/states/Cart.ts
@@ -3,16 +3,78 @@ import products from "../data/products";
 
 type state = {
   cart: {
+    // Key is product slug
     [key in string]: {
       quantity: number;
     };
   };
 };
 
+// Intializing and testing local storage
+const storage = window.localStorage;
+
+const testStorage = () => {
+  try {
+    var x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+};
+
+const storageAvailable = testStorage();
+
+// Transmition from state to storage and back
+const stateToStorage = (state: state) => {
+  if (storageAvailable) {
+    for (const key in products) {
+      // Map every available product so all wrong data get erased
+      storage.setItem(`cart--${key}`, `${state.cart[key]?.quantity || 0}`);
+    }
+  }
+};
+
+const stateFromStorage = (): state => {
+  const cart: { [key in string]: { quantity: number } } = {};
+  if (storageAvailable) {
+    for (const key in products) {
+      const item = storage.getItem(`cart--${products[key].slug}`);
+      if (item) {
+        cart[key] = { quantity: parseInt(item) };
+      }
+    }
+  }
+  return { cart };
+};
+
+// Reducer
+
 class Cart extends Container<state> {
-  state = { cart: {} };
+  state = stateFromStorage(); //Executes only on page load
+
+  // Synchronizes data to local storage after state is changed
+  setStateAndSync = async (data: ((state: state) => state) | state) => {
+    await this.setState(data);
+    stateToStorage(this.state);
+  };
+
   addItem = (item: string, quantity: number) => {
-    this.setState((state) => ({
+    this.setStateAndSync((state) => ({
       cart: {
         ...state.cart,
         [item]: {
@@ -25,7 +87,7 @@ class Cart extends Container<state> {
     }));
   };
   removeItem = (item: string) => {
-    this.setState((state) => ({
+    this.setStateAndSync((state) => ({
       cart: {
         ...state.cart,
         [item]: {
@@ -36,7 +98,7 @@ class Cart extends Container<state> {
   };
 
   increaseQuantity = (item: string) => {
-    this.setState((state) => ({
+    this.setStateAndSync((state) => ({
       cart: {
         ...state.cart,
         [item]: {
@@ -47,7 +109,7 @@ class Cart extends Container<state> {
   };
 
   decreaseQuantity = (item: string) => {
-    this.setState((state) => ({
+    this.setStateAndSync((state) => ({
       cart: {
         ...state.cart,
         [item]: {
@@ -61,7 +123,7 @@ class Cart extends Container<state> {
   };
 
   removeAll = () => {
-    this.setState({ cart: {} });
+    this.setStateAndSync({ cart: {} });
   };
 
   getPrices = () => {
