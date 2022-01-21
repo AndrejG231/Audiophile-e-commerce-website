26b2218460eefed9e1555b1fa2061c5e1f7377dc
Andrej Germič <andrej.germic@moderntv.eu>
2022-01-22 00:46:53 +0100

    chore: file paths and page url

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import BestGear from "../components/footer/BestGear";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header";
-import {
-  ZX9ProductView,
-  ZX7ProductView,
-  YX1ProductView,
-} from "../components/home_products";
+import { ZX9ProductView, ZX7ProductView, YX1ProductView } from "../components/home_products";
 import Navbar from "../components/navbar";
 import CategorySelection from "../components/home_category_select";
 
